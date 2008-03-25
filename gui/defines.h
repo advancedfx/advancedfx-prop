7b1f0e36a9b74641b8b02ffcead74c67df0f5491
Fabian <neomic@gmx.net>
2008-03-25 16:41:59 +0000

    away with this!

--- gui/defines.h
+++
@@ -1,8 +0,0 @@
-#ifndef HLAE_DEFINES_H
-#define HLAE_DEFINES_H
-
-#define HLAE_NAME "Half-Life Advanced Effects"
-#define HLAE_DEFAULTHLPATH "C:\\Program Files\\Steam\\SteamApps"
-#define HLAE_DLLNAME "Mirv Demo Tool.dll"
-
-#endif
