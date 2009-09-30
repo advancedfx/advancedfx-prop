c1a6113dc3b55a64979510b5add734acfdd1ed5a
Dominik Tugend <dominik@matrixstorm.com>
2009-09-30 15:24:30 +0000

    finally, hopefully

--- hooks/shared/detours.cpp
+++ hooks/shared/detours.cpp
@@ -10,6 +10,61 @@
 #define PUSH_EAX	0x50
 #define PUSH_ECX	0x51
 
+
+//#pragma warning(disable: 4312)
+//#pragma warning(disable: 4311)
+#define MakePtr(cast, ptr, addValue) (cast)((DWORD)(ptr) + (DWORD)(addValue))
+
+void *InterceptDllCall(HMODULE hModule, char *szDllName, char *szFunctionName, DWORD pNewFunction)
+{
+	PIMAGE_DOS_HEADER pDosHeader;
+	PIMAGE_NT_HEADERS pNTHeader;
+	PIMAGE_IMPORT_DESCRIPTOR pImportDesc;
+	PIMAGE_THUNK_DATA pThunk;
+	MdtMemBlockInfos mbis;
+	void *pOldFunction;
+
+
+	if (!(pOldFunction = GetProcAddress(GetModuleHandle(szDllName), szFunctionName)))
+		return NULL;
+
+	pDosHeader = (PIMAGE_DOS_HEADER) hModule;
+	if (pDosHeader->e_magic != IMAGE_DOS_SIGNATURE)
+		return NULL;
+
+	pNTHeader = MakePtr(PIMAGE_NT_HEADERS, pDosHeader, pDosHeader->e_lfanew);
+	if (pNTHeader->Signature != IMAGE_NT_SIGNATURE
+	|| (pImportDesc = MakePtr(PIMAGE_IMPORT_DESCRIPTOR, pDosHeader, pNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress)) == (PIMAGE_IMPORT_DESCRIPTOR) pNTHeader)
+		return NULL;
+
+	while (pImportDesc->Name)
+	{
+		char *szModuleName = MakePtr(char *, pDosHeader, pImportDesc->Name);
+		if (!stricmp(szModuleName, szDllName))
+			break;
+		pImportDesc++;
+	}
+	if (pImportDesc->Name == NULL)
+		return NULL;
+
+	pThunk = MakePtr(PIMAGE_THUNK_DATA, pDosHeader,	pImportDesc->FirstThunk);
+	while (pThunk->u1.Function)
+	{
+		if (pThunk->u1.Function == (DWORD)pOldFunction)
+		{
+			MdtMemAccessBegin((void *) &pThunk->u1.Function, sizeof(DWORD), &mbis);
+			pThunk->u1.Function = (DWORD) pNewFunction;
+			MdtMemAccessEnd(&mbis);
+
+			return pOldFunction;
+		}
+		pThunk++;
+	}
+
+	return NULL;
+}
+
+
 LPVOID MdtAllocExecuteableMemory(size_t size)
 {
 	DWORD dwDummy;
--- hooks/shared/detours.h
+++ hooks/shared/detours.h
@@ -14,6 +14,8 @@ typedef std::vector<MdtMemBlockInfo> MdtMemBlockInfos;
 void *DetourApply(BYTE *orig, BYTE *hook, int len);
 void *DetourClassFunc(BYTE *src, const BYTE *dst, const int len);
 
+void * InterceptDllCall(HMODULE hModule, char * szDllName, char * szFunctionName, DWORD pNewFunction);
+
 void MdtMemAccessBegin(LPVOID lpAddress, size_t size, MdtMemBlockInfos *mdtMemBlockInfos);
 void MdtMemAccessEnd(MdtMemBlockInfos *mdtMemBlockInfos);
 LPVOID MdtAllocExecuteableMemory(size_t size);
