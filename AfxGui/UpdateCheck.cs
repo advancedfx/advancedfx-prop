bb40f1b61a21ae0fb641cb6fc94bc9441e4f8175
Dominik Tugend <dominik@matrixstorm.com>
2010-09-10 10:09:02 +0000

    HLAE 2.2.19.2 AfxHookSource 0.0.3.8

--- AfxGui/UpdateCheck.cs
+++ AfxGui/UpdateCheck.cs
@@ -58,7 +58,7 @@ namespace AfxGui
 
             m_Guids = new Guid[]{
                 // always put the current guid first.
-				new Guid("e0d4d3be-c839-450a-80b1-2d1b8512b08e"),
+				new Guid("af3d2506-3aa0-436b-a82e-cfb0db975fcb"),
 				new Guid("a83c4d98-6be1-4eff-9793-f7bc0ecd9310")
             };
 
