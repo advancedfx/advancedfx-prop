9a02da470b58ffc8deeb38e08f484506ce59624d
Dominik Tugend <dominik@matrixstorm.com>
2019-04-12 18:37:58 +0200

    HLAE 2.63.5 (2019-04-03T05:51Z)

--- AfxHookSource/SourceInterfaces.h
+++ AfxHookSource/SourceInterfaces.h
@@ -6448,5 +6448,54 @@ public:
 	virtual panorama::CDebugger * CreateDebugger(panorama::CTopLevelWindowSource2 * topLevelWindow, const char * factory = "Debugger") = 0;
 };
 
+#define SOURCESDK_CSGO_VSERVERTOOLS_INTERFACE_VERSION "VSERVERTOOLS001"
+
+class IServerEntity;
+class CEntityRespawnInfo;
+
+class __declspec(novtable) IServerTools abstract : public IBaseInterface
+{
+public:
+	virtual IServerEntity *GetIServerEntity(IClientEntity_csgo *pClientEntity) = 0;
+	virtual bool SnapPlayerToPosition(const Vector &org, const QAngle &ang, IClientEntity_csgo *pClientPlayer = NULL) = 0;
+	virtual bool GetPlayerPosition(Vector &org, QAngle &ang, IClientEntity_csgo *pClientPlayer = NULL) = 0;
+	virtual bool SetPlayerFOV(int fov, IClientEntity_csgo *pClientPlayer = NULL) = 0;
+	virtual int GetPlayerFOV(IClientEntity_csgo *pClientPlayer = NULL) = 0;
+	virtual bool IsInNoClipMode(IClientEntity_csgo *pClientPlayer = NULL) = 0;
+
+	// entity searching
+	virtual void *FirstEntity(void) = 0;
+	virtual void *NextEntity(void *pEntity) = 0;
+	virtual void *FindEntityByHammerID(int iHammerID) = 0;
+
+	// entity query
+	virtual bool GetKeyValue(void *pEntity, const char *szField, char *szValue, int iMaxLen) = 0;
+	virtual bool SetKeyValue(void *pEntity, const char *szField, const char *szValue) = 0;
+	virtual bool SetKeyValue(void *pEntity, const char *szField, float flValue) = 0;
+	virtual bool SetKeyValue(void *pEntity, const char *szField, const Vector &vecValue) = 0;
+
+	// entity spawning
+	virtual void *CreateEntityByName(const char *szClassName) = 0;
+	virtual void DispatchSpawn(void *pEntity) = 0;
+	virtual bool DestroyEntityByHammerId(int iHammerID) = 0;
+
+	// This function respawns the entity into the same entindex slot AND tricks the EHANDLE system into thinking it's the same
+	// entity version so anyone holding an EHANDLE to the entity points at the newly-respawned entity.
+	virtual bool RespawnEntitiesWithEdits(CEntityRespawnInfo *pInfos, int nInfos) = 0;
+
+	// This reloads a portion or all of a particle definition file.
+	// It's up to the server to decide if it cares about this file
+	// Use a UtlBuffer to crack the data
+	virtual void ReloadParticleDefintions(const char *pFileName, const void *pBufData, int nLen) = 0;
+
+	virtual void AddOriginToPVS(const Vector &org) = 0;
+	virtual void MoveEngineViewTo(const Vector &vPos, const QAngle &vAngles) = 0;
+
+	// Call UTIL_Remove on the entity.
+	virtual void RemoveEntity(int nHammerID) = 0;
+
+	// one more we don't know.
+};
+
 } // namespace CSGO {
 } // namespace SOURCESDK {
