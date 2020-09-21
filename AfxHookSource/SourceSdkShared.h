0e3b83539165d4ab2065d9c8815403205a4124f3
Dominik Tugend <dominik@matrixstorm.com>
2020-09-21 10:45:11 +0200

    Prepare GOTV as POV advancedfx/advancedfx#384

--- AfxHookSource/SourceSdkShared.h
+++ AfxHookSource/SourceSdkShared.h
@@ -27,6 +27,7 @@ namespace SOURCESDK {
 typedef float vec_t;
 typedef signed char int8;
 typedef signed __int32 int32;
+typedef signed __int64 int64;
 typedef unsigned char uint8;
 typedef unsigned __int32 uint32;
 typedef unsigned __int16 uint16;
