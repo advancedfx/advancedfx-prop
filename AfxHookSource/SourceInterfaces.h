fddfe8e7fe5517958a989c8a6c1b3223bc774f2e
Dominik Tugend <dominik@matrixstorm.com>
2022-09-17 10:49:06 +0200

    Fix GameEvent interface.

--- AfxHookSource/SourceInterfaces.h
+++ AfxHookSource/SourceInterfaces.h
@@ -5635,18 +5635,22 @@ public:
 	virtual bool IsLocal() const = 0; // if event is never networked
 	virtual bool IsEmpty(const char *keyName = NULL) = 0; // check if data field exists
 
-														  // Data access
-	virtual bool GetBool(const char *keyName = NULL, bool defaultValue = false) = 0;
-	virtual int GetInt(const char *keyName = NULL, int defaultValue = 0) = 0;
-	virtual uint64 GetUint64(const char *keyName = NULL, uint64 defaultValue = 0) = 0;
-	virtual float GetFloat(const char *keyName = NULL, float defaultValue = 0.0f) = 0;
-	virtual const char *GetString(const char *keyName = NULL, const char *defaultValue = "") = 0;
+	// Data access
+	virtual bool  GetBool( const char *keyName = NULL, bool defaultValue = false ) = 0;
+	virtual int   GetInt( const char *keyName = NULL, int defaultValue = 0 ) = 0;
+	virtual uint64 GetUint64( const char *keyName = NULL, uint64 defaultValue = 0 ) = 0;
+	virtual float GetFloat( const char *keyName = NULL, float defaultValue = 0.0f ) = 0;
+	virtual const char *GetString( const char *keyName = NULL, const char *defaultValue = "" ) = 0;
+	virtual const wchar_t *GetWString(char const *keyName = NULL, const wchar_t *defaultValue = L"") = 0;
+	virtual const void *GetPtr( const char *keyname = NULL, const void *defaultValues = NULL ) = 0;
 
-	virtual void SetBool(const char *keyName, bool value) = 0;
-	virtual void SetInt(const char *keyName, int value) = 0;
-	virtual void SetUint64(const char *keyName, uint64 value) = 0;
-	virtual void SetFloat(const char *keyName, float value) = 0;
-	virtual void SetString(const char *keyName, const char *value) = 0;
+	virtual void SetBool( const char *keyName, bool value ) = 0;
+	virtual void SetInt( const char *keyName, int value ) = 0;
+	virtual void SetUint64( const char *keyName, uint64 value ) = 0;
+	virtual void SetFloat( const char *keyName, float value ) = 0;
+	virtual void SetString( const char *keyName, const char *value ) = 0;
+	virtual void SetWString( const char *keyName, const wchar_t *value ) = 0;
+	virtual void SetPtr( const char *keyname, const void *value ) = 0;
 };
 
 class CGameEventDescriptor;
