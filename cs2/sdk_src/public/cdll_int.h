9fe9be4e6f60f0ac090664557caafd11382d231a
Dominik Tugend <dominik@matrixstorm.com>
2023-09-24 16:43:49 +0200

    HLAE 2.148.4

--- cs2/sdk_src/public/cdll_int.h
+++ cs2/sdk_src/public/cdll_int.h
@@ -113,6 +113,26 @@ public:
     virtual IDemoFile* GetDemoFile(void) = 0; //:056
 };
 
+enum ClientFrameStage_t
+{
+	FRAME_UNDEFINED=-1,			// (haven't run any frames yet)
+	FRAME_START,
+
+	// A network packet is being recieved
+	FRAME_NET_UPDATE_START,
+		// Data has been received and we're going to start calling PostDataUpdate
+		FRAME_NET_UPDATE_POSTDATAUPDATE_START,
+		// Data has been received and we've called PostDataUpdate on all data recipients
+		FRAME_NET_UPDATE_POSTDATAUPDATE_END,
+	// We've received all packets, we can now do interpolation, prediction, etc..
+	FRAME_NET_UPDATE_END,		
+
+	// We're about to start rendering the scene
+	FRAME_RENDER_START,
+	// We've finished rendering the scene.
+	FRAME_RENDER_END
+};
+
 } // namespace CS2 {
 } // namespace SOURCESDK {
 
