c3e4a85dbc7c02da737ef2ec792cbcae7e91ad79
Dominik Tugend <dominik@matrixstorm.com>
2025-05-30 23:10:43 +0200

    Add CUtlString::Set

--- cs2/sdk_src/public/tier1/utlstring.cpp
+++ cs2/sdk_src/public/tier1/utlstring.cpp
@@ -8,6 +8,12 @@ namespace CS2 {
 
 extern HMODULE GetTier0DllHandle(); // utlmemory.cpp
 
+void CUtlString::Set(const char *pValue) {
+    static void ( __fastcall * pFn)(void*,const char *) = (void (*)(void*,const char *)) GetProcAddress(GetTier0DllHandle(),"?Set@CUtlString@@QEAAXPEBD@Z");
+    if(pFn) return pFn(this,pValue);
+    return;    
+}
+
 void CUtlString::Purge() {
     static void ( __fastcall * pFn)(void*) = (void (*)(void*)) GetProcAddress(GetTier0DllHandle(),"?Purge@CUtlString@@QEAAXXZ");
     if(pFn) return pFn(this);
