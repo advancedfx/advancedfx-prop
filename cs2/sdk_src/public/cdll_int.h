49dc76f45dbee4def35e0dcd22f14396f28b1c26
Dominik Tugend <dominik@matrixstorm.com>
2025-06-14 18:10:45 +0200

    Update ClientFrameStage_t

--- cs2/sdk_src/public/cdll_int.h
+++ cs2/sdk_src/public/cdll_int.h
@@ -123,22 +123,9 @@ public:
 
 enum ClientFrameStage_t
 {
-	FRAME_UNDEFINED=-1,			// (haven't run any frames yet)
-	FRAME_START,
-
-	// A network packet is being recieved
-	FRAME_NET_UPDATE_START,
-		// Data has been received and we're going to start calling PostDataUpdate
-		FRAME_NET_UPDATE_POSTDATAUPDATE_START,
-		// Data has been received and we've called PostDataUpdate on all data recipients
-		FRAME_NET_UPDATE_POSTDATAUPDATE_END,
-	// We've received all packets, we can now do interpolation, prediction, etc..
-	FRAME_NET_UPDATE_END,		
-
-	// We're about to start rendering the scene
-	FRAME_RENDER_START,
-	// We've finished rendering the scene.
-	FRAME_RENDER_END
+	FRAME_UNDEFINED=-1,     // (haven't run any frames yet)
+	FRAME_RENDER_PASS = 9   // Render a frame for display
+    // There are more values in-between, but their meanings have changed and we did not confirm them yet.
 };
 
 } // namespace CS2 {
