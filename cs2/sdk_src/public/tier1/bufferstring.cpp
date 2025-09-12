9ebfe95273783c11195093a8b5e90ec13864374d
ChetdeJong <floweplay@gmail.com>
2025-09-12 16:27:07 +0400

    fix: make pFn static in bufferstring

--- cs2/sdk_src/public/tier1/bufferstring.cpp
+++ cs2/sdk_src/public/tier1/bufferstring.cpp
@@ -10,40 +10,40 @@ extern HMODULE GetTier0DllHandle(); // utlmemory.cpp
 
 const char * CBufferString::Insert(int nIndex, const char *pBuf, int nCount, bool bIgnoreAlignment) {
 	typedef const char * (__fastcall * Insert_t)(void* This, int nIndex, const char *pBuf, int nCount, bool bIgnoreAlignment);
-    auto pFn = (Insert_t)GetProcAddress(GetTier0DllHandle(),"?Insert@CBufferString@@QEAAPEBDHPEBDH_N@Z");
+    static auto pFn = (Insert_t)GetProcAddress(GetTier0DllHandle(),"?Insert@CBufferString@@QEAAPEBDHPEBDH_N@Z");
     if(pFn) return pFn(this, nIndex, pBuf, nCount, bIgnoreAlignment);
 	return "";
 }
 
 void CBufferString::Purge(int nAllocatedBytesToPreserve) {
 	typedef const void (__fastcall * Insert_t)(void* This, int nAllocatedBytesToPreserve);
-    auto pFn = (Insert_t)GetProcAddress(GetTier0DllHandle(),"?Purge@CBufferString@@QEAAXH@Z");
+    static auto pFn = (Insert_t)GetProcAddress(GetTier0DllHandle(),"?Purge@CBufferString@@QEAAXH@Z");
     if(pFn) return pFn(this, nAllocatedBytesToPreserve);
 }
 
 const char * CBufferString::FixupPathName(char cSeparator) {
 	typedef const char* (__fastcall * FixupPathName_t)(void*, char);
-    auto pFn = (FixupPathName_t)GetProcAddress(GetTier0DllHandle(),"?FixupPathName@CBufferString@@QEAAPEBDD@Z");
+    static auto pFn = (FixupPathName_t)GetProcAddress(GetTier0DllHandle(),"?FixupPathName@CBufferString@@QEAAPEBDD@Z");
     if(pFn) return pFn(this, cSeparator);
 	return "";
 }
 
 void CBufferString::ToLowerFast(int nStart) {
 	typedef const char* (__fastcall * ToLowerFast_t)(void*, int);
-    auto pFn = (ToLowerFast_t)GetProcAddress(GetTier0DllHandle(),"?ToLowerFast@CBufferString@@QEAAXH@Z");
+    static auto pFn = (ToLowerFast_t)GetProcAddress(GetTier0DllHandle(),"?ToLowerFast@CBufferString@@QEAAXH@Z");
     if(pFn) pFn(this, nStart);
 }
 
 const char * CBufferString::FixSlashes(char cSeparator) {
 	typedef const char* (__fastcall * FixSlashes_t)(void*, char);
-    auto pFn = (FixSlashes_t)GetProcAddress(GetTier0DllHandle(),"?FixSlashes@CBufferString@@QEAAPEBDD@Z");
+    static auto pFn = (FixSlashes_t)GetProcAddress(GetTier0DllHandle(),"?FixSlashes@CBufferString@@QEAAPEBDD@Z");
     if(pFn) return pFn(this, cSeparator);
 	return "";
 }
 
 const char * CBufferString::ExtractFileExtension(const char *pPath) {
 	typedef const char* (__fastcall * ExtractFileExtension_t)(void*, const char*);
-    auto pFn = (ExtractFileExtension_t)GetProcAddress(GetTier0DllHandle(),"?ExtractFileExtension@CBufferString@@QEAAPEBDPEBD@Z");
+    static auto pFn = (ExtractFileExtension_t)GetProcAddress(GetTier0DllHandle(),"?ExtractFileExtension@CBufferString@@QEAAPEBDPEBD@Z");
     if(pFn) return pFn(this, pPath);
 	return "";
 }
